868755a124eccded4b0ef8c99cb512a47d5190d3
lining1111 <770677655@qq.com>
2025-02-14 10:24:38 +0800

    增加将查询结果存入结构体的代码

--- test/select/ch1.cpp
+++ test/select/ch1.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "dbCom/utils.h"
+#include "dbCom/mydata.hpp"
 
 // data/mydata.db---product
 /*
@@ -26,11 +27,22 @@ int main(int argc, char **argv) {
         stmt.reset(session);
         //3、操作
         // 查询整体的结果
-        if (0){
-            stmt << "select * from product;";
+        if (1){
+            stmt << "select name,price,amount,categoryID,notes from product;";
             stmt.execute();
             RecordSet rs(stmt);
             showResult(rs);
+            //将结果存入到结构体内
+            vector<mydata::Product> products;
+            for (auto iter:rs) {
+                mydata::Product item;
+                item.name = iter.get(0).toString();
+                item.price = strtof(iter.get(1).toString().c_str(), nullptr);
+                item.amount = strtol(iter.get(2).toString().c_str(), nullptr,10);
+                item.categoryID  = iter.get(3).toString();
+                item.notes = iter.get(4).toString();
+                products.push_back(item);
+            }
         }
 
 
@@ -42,7 +54,7 @@ int main(int argc, char **argv) {
             showResult(rs1);
         }
         //查看价格大于1的并且/或者存储数量大于600的 where xxx and/or xxx
-        if (1){
+        if (0){
 //            stmt <<"select * from product where price > 2 and amount > 600;";
             stmt <<"select * from product where price > 2 or amount > 600;";
             stmt.execute();
